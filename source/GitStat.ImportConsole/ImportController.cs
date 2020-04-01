151f137a3f93f4b32bf8a39fde52d4fe3b794eeb
Marcel Flieger <fliegermar@gmail.com>
2020-04-01 18:05:10 +0200

    Implements ImportController

--- source/GitStat.ImportConsole/ImportController.cs
+++ source/GitStat.ImportConsole/ImportController.cs
@@ -17,8 +17,95 @@ namespace GitStat.ImportConsole
         /// </summary>
         public static Commit[] ReadFromCsv()
         {
-            throw new NotImplementedException();
+            List<Commit> commits = new List<Commit>();
+            string[] lines = File.ReadAllLines(MyFile.GetFullNameInApplicationTree(Filename));
+            Dictionary<string, Developer> developers = new Dictionary<string, Developer>();
+
+            bool isHeader = true;
+            Developer dev = null;
+            string hashcode;
+            DateTime dateTime;
+            string message = null;
+            int fileChanges = 0;
+            int inserts = 0;
+            int deletes = 0;
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(",");
+
+                if (parts.Length > 1)
+                {
+                    if (parts.Length >= 5)
+                    {
+                        if(!isHeader)
+                        {
+                            Commit newCommit = new Commit()
+                            {
+                                HashCode = parts[0],
+                                Developer = dev,
+                                Date = Convert.ToDateTime(parts[2]),
+                                Message = parts[3],
+                                FilesChanges = fileChanges,
+                                Insertions = inserts,
+                                Deletions = deletes
+                            };
+
+                            commits.Add(newCommit);
+                            isHeader = true;
+                        }
+                        if (!developers.ContainsKey(parts[1]))
+                        {
+                            dev = new Developer() { Name = parts[1] };
+                            developers.Add(parts[1], dev);
+                        }
+                        else
+                        {
+                            dev = developers.GetValueOrDefault(parts[1]);
+                        }
+
+                        hashcode = parts[0];
+                        dateTime = Convert.ToDateTime(parts[2]);
+
+                        for (int i = 5; i < parts.Length; i++)
+                        {
+                            message = $"{message}, {parts[i]}";
+                        }
+
+                        isHeader = false;
+                    }
+
+                    if(parts.Length <= 3)
+                    {
+                            fileChanges = GetFirstValueOfString(parts[0]);
+
+                            if (parts.Length == 3)
+                            {
+                                inserts = GetFirstValueOfString(parts[1]);
+                                deletes = GetFirstValueOfString(parts[2]);
+                            }
+                            else if (parts[1].Contains("insertion"))
+                            {
+                                inserts = GetFirstValueOfString(parts[1]);
+                            }
+                            else if (parts[1].Contains("deletion"))
+                            {
+                                deletes = GetFirstValueOfString(parts[1]);
+                            }                        
+                    }
+                }
+
+            }
+
+
+            return commits.ToArray();
         }
 
+        private static int GetFirstValueOfString(string input)
+        {
+            string[] parts = input.Split(" ");
+
+            return Convert.ToInt32(parts[1]);
+        }
     }
 }
